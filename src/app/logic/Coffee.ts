e548011b08c04c56fe34920f70932624ae84a9af
gitumar <umarb8@yahoo.com>
2018-01-31 18:53:33 -0600

    First commit

---
+++ src/app/logic/Coffee.ts
@@ -0,0 +1,11 @@
+class Coffee{
+
+    //Properties Here
+    name: string;
+    place: string;
+    type: string;
+    location: PlaceLocation;
+    rating: number;
+    notes: string;
+    tastingRating: TastingRating;
+}
